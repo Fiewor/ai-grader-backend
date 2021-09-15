f942998926304cc33566690a9d0781a01c225c12
Liviu Blidar <liviu.blidar94@gmail.com>
2021-09-15 08:47:25 +0100

    file upload ready

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express')
-
+const multer = require("multer");
 const app = express()
 const port = process.env.PORT||3000
+const path = require('path')
+
+// handle uploads:
+const uploadStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/'); //store in uploads dir
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)); //Appending extension because multer doesn't do it by default, doh
+    }
+});
+
+const upload = multer({storage: uploadStorage});
+
+
+
 
 app.get('/', (req, res)=>{
     res.send(`Working`)
@@ -10,9 +26,8 @@ app.get('/', (req, res)=>{
 app.post('/login', (req, res)=>{
     res.send(`login working!`)
 })
-app.post('/upload', (req, res)=>{
-    console.log(req.body)
-    res.send(`upload working!`)
+app.post('/upload', upload.single('uploaded_file'),(req, res)=>{
+    res.send(`upload working!, check uploads folder in the project`)
 })
 
 app.listen(port, ()=>{
