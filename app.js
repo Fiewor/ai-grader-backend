b09b28317c140b3e16c19ac7cba0538a6f40161a
Liviu Blidar <liviu.blidar94@gmail.com>
2021-09-18 23:00:50 +0100

    handle file upload via expres-fileupload instead of multer

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
 const express = require('express')
-const multer = require("multer");
 const app = express()
-const port = process.env.PORT||3000
-const path = require('path')
-
-// handle uploads:
-const uploadStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads/'); //store in uploads dir
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)); //Appending extension because multer doesn't do it by default, doh
-    }
-});
-
-const upload = multer({storage: uploadStorage});
-
-
-
+const port = process.env.PORT||3001
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
 
 app.get('/', (req, res)=>{
-    res.send(`Working`)
+    res.send(`Working`);
 })
 
 app.post('/login', (req, res)=>{
-    res.send(`login working!`)
+    res.send(`login working!`);
 })
-app.post('/upload', upload.single('uploaded_file'),(req, res)=>{
-    res.send(`upload working!, check uploads folder in the project`)
+app.post('/upload',(req, res)=>{
+    for(let file of Object.values(req.files)) {
+        let pathToFile = __dirname + "/uploads/" + file.name;
+
+        file.mv(pathToFile, (err) => {
+            if (err) {
+                console.log('and error is ', err);
+            }
+        });
+    }
+
+    res.send(`upload working!, check uploads folder in the project`);
 })
 
-app.listen(port, ()=>{
-    console.log(`Server is started on port ${port}`)
+app.listen(port, () => {
+    console.log(`Server is started on port ${port}`);
 })
