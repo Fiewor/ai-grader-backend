a5df13167649b66b4fb4551985a5f3fbb191348b
John Fiewor <johnfiewor@gmail.com>
2021-09-28 23:54:55 +0100

    add debug, standard linter and attempt to fix cors error

--- app.js
+++ app.js
@@ -1,35 +1,68 @@
 const express = require('express')
-const multer = require("multer");
+const fileUpload = require('express-fileupload')
+// const multer = require("multer");
 const app = express()
-const port = process.env.PORT||3000
-const path = require('path')
+const port = process.env.PORT || 3001
+// const path = require('path')
+
+app.use(fileUpload())
+
+let allowCrossDomain = function(req, res, next){
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Tpye');
+    next();
+}
+app.use(allowCrossDomain)
 
 // handle uploads:
-const uploadStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads/'); //store in uploads dir
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)); //Appending extension because multer doesn't do it by default, doh
+// const uploadStorage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, './uploads/'); //store in uploads dir
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, Date.now() + path.extname(file.originalname)); //Appending extension because multer doesn't do it by default, doh
+//     }
+// });
+
+// const upload = multer({ storage: uploadStorage });
+
+app.get('/', (req, res) => {
+  res.send('Working')
+})
+
+app.post('/login', (req, res) => {
+  res.send('login working!')
+})
+
+app.post('/upload', (req, res) => {
+//   console.log(req.files.name)
+  let file
+  let uploadPath
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.')
+  }
+
+  file = req.files.file
+  console.log(file)
+  uploadPath = __dirname + '/uploads' + file.name
+  console.log(uploadPath)
+
+  //  the mv() method is used to place the file somewhere on the server
+  file.mv(uploadPath, err => {
+    if (err) {
+      return res.status(500).send(err)
     }
-});
-
-const upload = multer({storage: uploadStorage});
-
-
-
-
-app.get('/', (req, res)=>{
-    res.send(`Working`)
+    res.send('File uploaded')
+  })
 })
 
-app.post('/login', (req, res)=>{
-    res.send(`login working!`)
-})
-app.post('/upload', upload.single('uploaded_file'),(req, res)=>{
-    res.send(`upload working!, check uploads folder in the project`)
-})
+// app.post('/upload', upload.single('uploaded_file'), (req, res) => {
+//   console.log(req)
+//   res.send('upload working!, check uploads folder in the project')
+// })
 
-app.listen(port, ()=>{
-    console.log(`Server is started on port ${port}`)
+app.listen(port, () => {
+  console.log(`Server is started on port ${port}`)
 })
