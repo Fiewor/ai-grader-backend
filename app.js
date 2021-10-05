504d89e24f7cdcc2cc9f57b8a969328fd2362b7b
John Fiewor <johnfiewor@gmail.com>
2021-10-05 11:59:13 +0100

    add azure dependencies and ignore .env

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+'use strict';
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT||3001
 const fileUpload = require('express-fileupload');
+
+const async = require('async');
+const fs = require('fs');
+const https = require('https');
+const path = require("path");
+const createReadStream = require('fs').createReadStream
+const sleep = require('util').promisify(setTimeout);
+const ComputerVisionClient = require('@azure/cognitiveservices-computervision').ComputerVisionClient;
+const ApiKeyCredentials = require('@azure/ms-rest-js').ApiKeyCredentials;
+
+/**
+ * AUTHENTICATE
+ * This single client is used for all examples.
+ */
+
+
 app.use(fileUpload());
 
 app.use((req, res, next) => {
