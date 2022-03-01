fe7dc260fee310209f721358b7e5c8db1a16cf6b
Fiewor John <johnfiewor@gmail.com>
2022-03-01 08:28:14 +0100

    feat: add mongodb atlas and seperate db code

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-'use strict';
+"use strict";
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT||3001
-const fileUpload = require('express-fileupload');
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+const fileUpload = require("express-fileupload");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const fs = require('fs');
+const fs = require("fs");
 const path = require("path");
-const createReadStream = fs.createReadStream
-const sleep = require('util').promisify(setTimeout);
-const ComputerVisionClient = require('@azure/cognitiveservices-computervision').ComputerVisionClient;
-const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics");
-const ApiKeyCredentials = require('@azure/ms-rest-js').ApiKeyCredentials;
+const createReadStream = fs.createReadStream;
+const sleep = require("util").promisify(setTimeout);
+const ComputerVisionClient =
+  require("@azure/cognitiveservices-computervision").ComputerVisionClient;
+const {
+  TextAnalyticsClient,
+  AzureKeyCredential,
+} = require("@azure/ai-text-analytics");
+const ApiKeyCredentials = require("@azure/ms-rest-js").ApiKeyCredentials;
 
 const key = process.env.API_KEY;
 const endpoint = process.env.API_ENDPOINT;
@@ -21,161 +25,158 @@ const endpoint = process.env.API_ENDPOINT;
 const text_key = process.env.TEXT_KEY;
 const text_endpoint = process.env.TEXT_ENDPOINT;
 
-const textAnalyticsClient = new TextAnalyticsClient(text_endpoint,  new AzureKeyCredential(text_key));
+const textAnalyticsClient = new TextAnalyticsClient(
+  text_endpoint,
+  new AzureKeyCredential(text_key)
+);
 
 const computerVisionClient = new ComputerVisionClient(
-    new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } }), endpoint);
+  new ApiKeyCredentials({ inHeader: { "Ocp-Apim-Subscription-Key": key } }),
+  endpoint
+);
+
+// import grader from "./grading";
+// import main from "./db";
 
 let textArray = [];
 
 app.use(fileUpload());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  next()
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 // app.get('/', (req, res)=>{
 //     res.send(`Working`);
 // })
-// post handler
-const postHandler = folder => {
-    app.post(`/upload/${folder}/`,(req, res)=>{
-        for(let file of Object.values(req.files)) {
-            let pathToFile = `${__dirname}/uploads/${folder}/` + file.name;
 
-            file.mv(pathToFile, (err) => {
-                if (err) {
-                    console.log('and error is ', err);
-                }
-            });
+// post handler
+const postHandler = async (folder) => {
+  app.post(`/upload/${folder}/`, (req, res) => {
+    for (let file of Object.values(req.files)) {
+      let pathToFile = `${__dirname}/uploads/${folder}/` + file.name;
+
+      file.mv(pathToFile, (err) => {
+        if (err) {
+          console.log("and error is ", err);
         }
-        res.send(`${folder} sheet(s) uploaded successfully! Check ${folder} folder in the project's uploads directory`);
-    })
-}
-postHandler(`answer`)
-postHandler(`mark`)
+      });
+    }
+    res.send(
+      `${folder} sheet(s) uploaded successfully! Check ${folder} folder in the project's uploads directory`
+    );
+  });
+};
+postHandler(`answer`);
+postHandler(`mark`);
 
 app.listen(port, () => {
-    console.log(`Server is started on port ${port}`);
-})
+  console.log(`Server is started on port ${port}`);
+});
 
+let completeText;
 // function to extract identifiable text from image. takes image path as argument
-let getTextFromImage = async (imagePath) => {    
-    const STATUS_SUCCEEDED = "succeeded";
-    const STATUS_FAILED = "failed"
-    
-    console.log('\Reading local image for text in ...', path.basename(imagePath));
-    
-    const streamResponse = await computerVisionClient.readInStream(() => createReadStream(imagePath))
-        .then(response => response)
-        .catch(err => console.error(err))
+let getTextFromImage = async (imagePath) => {
+  const STATUS_SUCCEEDED = "succeeded";
+  const STATUS_FAILED = "failed";
 
-    // Get operation location from response, so you can get the operation ID.
-    const operationLocationLocal = streamResponse.operationLocation
-    // Get the operation ID at the end of the URL
-    const operationIdLocal = operationLocationLocal.substring(operationLocationLocal.lastIndexOf('/') + 1);
-        
-    // Wait for the read operation to finish, use the operationId to get the result.
-    while (true) {
-        const readOpResult = await computerVisionClient.getReadResult(operationIdLocal)
-            .then((result) => {
-                return result;
-            })
-        console.log('Read status: ' + readOpResult.status)
+  console.log("Reading local image for text in ...", path.basename(imagePath));
 
-        if (readOpResult.status === STATUS_FAILED) {
-            console.log('The Read File operation has failed.')
-            break;
-        }
-        if (readOpResult.status === STATUS_SUCCEEDED) {
-            console.log('The Read File operation was a success.');
-            console.log();
-            console.log('Read File local image result:');
-            // Print the text captured
-            for (const textRecResult of readOpResult.analyzeResult.readResults) {
-                for (const line of textRecResult.lines) {
-                    textArray.push(line.text)
-                }
-                let completeText = textArray.join(' ')
-                console.log(`Printing captured text from getTextFromImage function: ${completeText}`)
-            }
-            break;
-        }
-        await sleep(1000);
+  const streamResponse = await computerVisionClient
+    .readInStream(() => createReadStream(imagePath))
+    .then((response) => response)
+    .catch((err) => console.error(err));
+
+  // Get operation location from response, so you can get the operation ID.
+  const operationLocationLocal = streamResponse.operationLocation;
+  console.log("operationLocationLocal", operationLocationLocal);
+  // Get the operation ID at the end of the URL
+  const operationIdLocal = operationLocationLocal.substring(
+    operationLocationLocal.lastIndexOf("/") + 1
+  );
+  // Wait for the read operation to finish, use the operationId to get the result.
+  while (true) {
+    const readOpResult = await computerVisionClient
+      .getReadResult(operationIdLocal)
+      .then((result) => result);
+    console.log("Read status: " + readOpResult.status);
+
+    if (readOpResult.status === STATUS_FAILED) {
+      console.log("The Read File operation has failed.");
+      break;
     }
-    return textArray
-}
+    if (readOpResult.status === STATUS_SUCCEEDED) {
+      console.log("The Read File operation was a success.");
+      console.log();
+      console.log("Read File local image result:");
+      // Print the text captured
+      for (const textRecResult of readOpResult.analyzeResult.readResults) {
+        for (const line of textRecResult.lines) {
+          textArray.push(line.text);
+        }
+        completeText = textArray.join(" ");
+        console.log(
+          `Printing captured text from getTextFromImage function: ${completeText}`
+        );
+      }
+      break;
+    }
+    await sleep(1000);
+  }
+  return textArray;
+};
 
 app.get(`http://localhost:3001/upload/mark`, (req, res) => {
-    // read file and extract text from marking guide
-    res.send(readOperation(`${__dirname}\\uploads\\mark`))
-    // .then(data => console.log("result of read operation",data))
-    // .catch((err) => console.log(err))
-})
+  // read file and extract text from marking guide
+  res.send(readOperation(`${__dirname}\\uploads\\mark`));
+  // .then(data => console.log("result of read operation",data))
+  // .catch((err) => console.log(err))
+});
 
-let extracted = []
+let extracted = [];
 // function to extract key phrases from provided text string
 let keyPhraseExtraction = async (client, keyPhrasesInput) => {
-    try {
-        const keyPhraseResult = await client.extractKeyPhrases(keyPhrasesInput);
-    
-        keyPhraseResult.forEach(document => {
-            console.log(`ID: ${document.id}`);
-            console.log(`\tDocument Key Phrases: ${document.keyPhrases}`);
-            extracted.push(document.keyPhrases)
-        });
-    } catch(e) {
-        console.log(e);
-    }
-    return extracted
-}
+  try {
+    const keyPhraseResult = await client.extractKeyPhrases(keyPhrasesInput);
 
-let markKeyPhrase, answerKeyPhrase
-const readOperation = path => {
-    fs.readdir(path, (err, files) => {
-        if (err) console.log(err)
-        
-        files.forEach((file) => {
-            getTextFromImage(`${__dirname}/uploads/mark//${file}`)
-            .then((results) => {
-                return keyPhraseExtraction(textAnalyticsClient, results);
-            })
-            .then((data) => {
-                markKeyPhrase = data
-                // console.log(`markKeyPhrase: ${markKeyPhrase}`)
-            })
-            .catch((err) => console.log(err))
-        });
-        return markKeyPhrase
+    keyPhraseResult.forEach((document) => {
+      console.log(`ID: ${document.id}`);
+      console.log(`\tDocument Key Phrases: ${document.keyPhrases}`);
+      extracted.push(document.keyPhrases);
     });
-}
+  } catch (e) {
+    console.log(e);
+  }
+  return extracted;
+};
 
+let markKeyPhrase, answerKeyPhrase;
+const readOperation = async (path) => {
+  fs.readdir(path, (err, files) => {
+    if (err) console.log(err);
+
+    files.forEach((file) => {
+      getTextFromImage(`${__dirname}/uploads/mark//${file}`)
+        .then((results) => {
+          return keyPhraseExtraction(textAnalyticsClient, results);
+        })
+        .then((data) => {
+          markKeyPhrase = data;
+          // console.log(`markKeyPhrase: ${markKeyPhrase}`)
+        })
+        .catch((err) => console.log(err));
+    });
+    return markKeyPhrase;
+  });
+};
 
 // read file and extract text from answer sheet
-readOperation(`${__dirname}\\uploads\\answer`)
+readOperation(`${__dirname}\\uploads\\answer`);
 //  .......
 
-// some code to compare markKeyPhrase and answerKeyPhrase
-let grade = 0
-const grader = (array1, array2) => {
-    for (let i = 0; i <= array1.length; i++) {
-        if(array1[i] === array2[i]) grade += 1
-    }
-}
-// grader(markKeyPhrase, answerKeyPhrase)
+// grading code
+// grader(markKeyPhrase, answerKeyPhrase);
 
-// database code
-// const mongoose = require('mongoose')
-// mongoose.connect("mongodb://localhost:27017/textExtract")
-// const textSchema = new mongoose.Schema({
-//     readText: String,
-//     keyPhrases: String
-// })
-// const Text = mongoose.model('Text', textSchema)
-
-// const text = new Text({
-//     readText: completeText,
-//     keyPhrases: answerKeyPhrase
-// })
-// text.save()
+// db code
+// main();
