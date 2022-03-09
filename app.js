400d1dd36059a7ed2bea3f4c24c95a9f9daefd9a
Fiewor John <johnfiewor@gmail.com>
2022-03-09 23:09:37 +0100

    fix: create helper functions to reduce code repitition

--- app.js
+++ app.js
@@ -43,6 +43,10 @@ let textArray = [];
 
 app.use(fileUpload());
 
+app.listen(port, () => {
+  console.log(`Server is started on port ${port}`);
+});
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
@@ -51,24 +55,42 @@ app.use((req, res, next) => {
 // post handler
 
 app.post(`/upload/mark/`, (req, res) => {
-  for (let file of Object.values(req.files)) {
-    let pathToFile = `${__dirname}/uploads/mark/` + file.name;
-
-    file.mv(pathToFile, (err) => {
-      if (err) {
-        console.log("and error is ", err);
-      }
-    });
-  }
-  // postHandler(req, mark);
+  postHandler(req, "mark");
   res.send(
     `mark sheet(s) uploaded successfully! Check mark folder in the project's uploads directory`
   );
 });
 
 app.post(`/upload/answer/`, (req, res) => {
+  postHandler(req, answer);
+  res.send(
+    `answer sheet(s) uploaded successfully! Check answer folder in the project's uploads directory`
+  );
+});
+
+app.get(`/viewText`, async (req, res) => {
+  try {
+    // read file and extract text from answer sheet
+    readOperation(`${__dirname}\\uploads\\answer`).then(() =>
+      db("answerText", answerKeyPhrase)
+    );
+
+    // read file and extract text from mark sheet
+    readOperation(`${__dirname}\\uploads\\mark`).then(() =>
+      db("markText", markKeyPhrase)
+    );
+
+    const result = await Text.find();
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// ! TO-DO: fix this to use to handle post requests
+const postHandler = async (req, folder) => {
   for (let file of Object.values(req.files)) {
-    let pathToFile = `${__dirname}/uploads/answer/` + file.name;
+    let pathToFile = `${__dirname}/uploads/${folder}/` + file.name;
 
     file.mv(pathToFile, (err) => {
       if (err) {
@@ -76,28 +98,7 @@ app.post(`/upload/answer/`, (req, res) => {
       }
     });
   }
-  // postHandler(req, answer);
-  res.send(
-    `answer sheet(s) uploaded successfully! Check answer folder in the project's uploads directory`
-  );
-});
-
-// ! TO-DO: fix this to use to handle post requests
-// const postHandler = async (req, folder) => {
-//   for (let file of Object.values(req.files)) {
-//     let pathToFile = `${__dirname}/uploads/${folder}/` + file.name;
-
-//     file.mv(pathToFile, (err) => {
-//       if (err) {
-//         console.log("and error is ", err);
-//       }
-//     });
-//   }
-// };
-
-app.listen(port, () => {
-  console.log(`Server is started on port ${port}`);
-});
+};
 
 let completeText;
 // function to extract identifiable text from image. takes image path as argument
@@ -148,17 +149,6 @@ let getTextFromImage = async (imagePath) => {
   return textArray;
 };
 
-app.get(`/viewText`, async (req, res) => {
-  try {
-    // read file and extract text from answer sheet
-    readOperation(`${__dirname}\\uploads\\answer`);
-    const result = await Text.find();
-    res.send(result);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
 let extracted = [];
 // function to extract key phrases from provided text string
 let keyPhraseExtraction = async (client, keyPhrasesInput) => {
@@ -187,30 +177,45 @@ const readOperation = async (path) => {
           return keyPhraseExtraction(textAnalyticsClient, results);
         })
         .then((data) => {
-          markKeyPhrase = data;
+          // ${file}KeyPhrase = data;
           // console.log(`markKeyPhrase: ${markKeyPhrase}`)
         })
-        .then(() => {
-          const db = async () => {
-            try {
-              const text = new Text({
-                readText: completeText,
-              });
-              text.keyPhrases.push(...markKeyPhrase[0]); // ! TODO: fix to push all key phrases, not just the first
-              await text.save();
+        // .then(() => {
+        // ! isolate this db function!
+        //   const db = async () => {
+        //     try {
+        //       const text = new Text({
+        //         readText: completeText,
+        //       });
+        //       text.keyPhrases.push(...markKeyPhrase[0]); // ! TODO: fix to push all key phrases, not just the first
+        //       await text.save();
 
-              console.log("saved data: ", text);
-            } catch (e) {
-              console.log(e.message);
-            }
-          };
-          db();
-        })
+        //       console.log("saved data: ", text);
+        //     } catch (e) {
+        //       console.log(e.message);
+        //     }
+        //   };
+        //   db();
+        // })
         .catch((err) => console.log(err));
     });
 
     return markKeyPhrase;
   });
+};
+
+const db = async (newDoc, keyPh) => {
+  try {
+    const { newDoc } = new Text({
+      readText: completeText,
+    });
+    `${newDoc}`.keyPhrases.push(...`${keyPh}`[0]);
+    await `${newDoc}`.save();
+
+    console.log("saved data: ", `${newDoc}`);
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
 // grading code
