94e35bdd3ce93aa8f07fef2fbb1f9c52c248d403
Fiewor John <johnfiewor@gmail.com>
2022-06-20 00:58:11 +0100

    fix: specify origin to allow cors

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(fileUpload());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", ["https://www.aigrader.live"]);
   res.header("Access-Control-Request-Methods", "*");
   res.header(
     "Access-Control-Allow-Methods",
