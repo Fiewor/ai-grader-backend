b81d2d201f928f6f9f432f3f27df0aba2cd7c7d7
Fiewor John <johnfiewor@gmail.com>
2022-06-22 07:20:03 +0100

    refactor: change response header to allow all origins and remove other header settings

--- app.js
+++ app.js
@@ -35,16 +35,7 @@ app.use(fileUpload());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", ["https://www.aigrader.live"]);
-  res.header("Access-Control-Request-Methods", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Origin", ["*"]);
   next();
 });
 
@@ -61,7 +52,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.post(`/uploads/mark/`, async (req, res) => {
-  if (req.files === null || undefined) {
+  if (req.files === null || req.files === undefined) {
     res.json({ noFile: true });
     return;
   }
