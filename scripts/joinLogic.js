abf009f242ebe0bcc09e17744ae28e06090bea1c
Fiewor John <johnfiewor@gmail.com>
2022-07-06 11:55:28 +0100

    fix(temp): regex for splitting text

--- scripts/joinLogic.js
+++ scripts/joinLogic.js
@@ -1,14 +1,14 @@
 // this logic joins text that are part of the same answer
 // but are identified/read to be on seperate lines by OCR
 const testArray = [
-  "1. Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+  "10) Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
   "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
   "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
 
-  "a. Ornare quam viverra orci sagittis eu volutpat odio facilisis",
+  "a) Ornare quam viverra orci sagittis eu volutpat odio facilisis",
   "Turpis egestas pretium aenean pharetra magna ac placerat",
 
-  "i) Non blandit massa enim nec dui nunc mattis enim",
+  "ii) Non blandit massa enim nec dui nunc mattis enim",
   "Nunc lobortis mattis aliquam faucibus purus in massa",
 ];
 
@@ -17,12 +17,15 @@ const joinSame = (arr) => {
 
   const splitByDelimiter = arr
     .reduce((currString, nextString) => `${currString} ${nextString}`)
-    .split(/(\d|\w){1}[.|)]/gi)
+    // .split(/(\d|\w){1}[.|)]/gi)
+    .split(/(\d|\w){1}[)]/gi)
     .filter((string) => string !== "");
+
+  console.log(splitByDelimiter);
 
   for (let i = 0; i < splitByDelimiter.length; i++) {
     (i === 0 || i % 2 === 0) &&
-      finalArr.push(`${splitByDelimiter[i]}. ${splitByDelimiter[i + 1]}`);
+      finalArr.push(`${splitByDelimiter[i]}) ${splitByDelimiter[i + 1]}`);
   }
 
   return finalArr;
