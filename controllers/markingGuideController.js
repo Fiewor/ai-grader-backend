9d172c7b90c2ac77cbe198754f2f274628929334
Fiewor <johnfiewor@gmail.com>
2022-10-27 14:24:59 +0100

    feat: controller to display marking guides in db

---
+++ controllers/markingGuideController.js
@@ -0,0 +1,31 @@
+const { MarkSheet } = require("../models/textModel");
+
+// @desc    Get all text
+// @route   GET /api/texts
+// @access  Public
+const getAllGuides = async (req, res) => {
+  try {
+    // extract all documents from DB
+    const doc = await MarkSheet.find({}, { page: { fileName: 1 } });
+
+    !doc ? res.send(["Empty"]) : res.send(doc);
+  } catch (err) {
+    console.log(err.stack);
+  }
+};
+
+// @desc    Get text with Id
+// @route   GET /api/texts/:id
+// @access  Public
+const getGuidesWithId = async (req, res) => {
+  let { id } = req.params;
+  try {
+    const doc = await MarkSheet.findById(id);
+
+    res.send(doc);
+  } catch (err) {
+    console.log(err.stack);
+  }
+};
+
+module.exports = { getAllGuides, getGuidesWithId };
