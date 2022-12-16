725734d574dd622d20f4eb6db4090d2e26cfc502
Fiewor <johnfiewor@gmail.com>
2022-12-17 00:00:36 +0100

    refactor: remove unused code

--- config/db.js
+++ config/db.js
@@ -3,7 +3,6 @@ const uri =
   process.env.NODE_ENV === "production"
     ? process.env.MONGODB_URI
     : `mongodb://localhost:27017/textExtract`;
-// ? `mongodb+srv://john:${process.env.MONGODB_ATLAS_KEY}@cluster0.crs9nx1.mongodb.net/?retryWrites=true&w=majority`
 
 const connectDB = async () => {
   try {
